0a8c2870b74da3fea922d5735d3706e806801803
Tatarcuka <kristiana.tatarcuka@accenture.com>
2023-11-06 12:29:15 +0200

    create yourMovies functionality

--- Scripts/home.js
+++ Scripts/home.js
@@ -49,67 +49,84 @@ const movieList = [
     },
 
 ];
-const yourMovies = [];
+let yourMovies = [];
 
 const table = document.querySelector(".content-table tbody");
 
-movieList.forEach((movie) => {
-    const row = document.createElement("tr");
-    
-    const nameCell = document.createElement("td");
-    nameCell.textContent = movie.movieName;
-    
-    const genreCell = document.createElement("td");
-    genreCell.textContent = movie.movieGenre;
-    
-    const priceCell = document.createElement("td");
-    priceCell.textContent = movie.rentalPrice.toFixed(2) + "€";
-
-
-    const stockCell = document.createElement("td");
-    const stockImage = document.createElement("img");
-    stockImage.src = movie.countInStock > 0 ? 
-    "../img/check.png" : "../img/Cross.png";
-    stockImage.alt = movie.countInStock <= 0 ?
-    "in stock" : "out of stock";
-    stockCell.appendChild(stockImage);
-
-    const rentCell = document.createElement("td");
-    const rentButton = document.createElement("button");
-    rentButton.className = "rent-btn";
-
-    if (movie.countInStock > 0) {
-        rentButton.textContent = "Rent";
-        rentButton.addEventListener("click", () => {
+function renderMovieList (movieList) {
+    movieList.forEach((movie) => {
+        const row = document.createElement("tr");
             
-            const index = movieList.findIndex((m) => m.movieName === movie.movieName);
-            console.log(index);
-            if (index !== -1 && movieList[index].countInStock > 0) {
-                movieList[index].countInStock--;
-                yourMovies.push(movie);
-                if (movieList[index].countInStock == 0) {
-                    rentButton.textContent = "Out of stock";
-                    rentButton.classList.add("out-of-stock");
-                    rentButton.disabled = true;
-                    stockImage.src = "../img/Cross.png";
-                }
-            }
-        })
+        const nameCell = document.createElement("td");
+        nameCell.textContent = movie.movieName;
+        
+        const genreCell = document.createElement("td");
+        genreCell.textContent = movie.movieGenre;
+        
+        const priceCell = document.createElement("td");
+        priceCell.textContent = movie.rentalPrice.toFixed(2) + "€";
+    
+        const stockCell = document.createElement("td");
+        const stockImage = document.createElement("img");
+        stockImage.src = movie.countInStock > 0 ? 
+        "../img/check.png" : "../img/Cross.png";
+        stockImage.alt = movie.countInStock <= 0 ?
+        "in stock" : "out of stock";
+        stockCell.appendChild(stockImage);
+    
+        const rentCell = document.createElement("td");
+        const rentButton = document.createElement("button");
+        rentButton.className = "rent-btn";
+
+        if (movie.countInStock > 0) {
+            rentButton.textContent = "Rent";
+            rentButton.addEventListener("click", () => {
+                handleRentClick(movie, rentButton, stockImage)
+            })
+        } else {
+            rentButton.textContent = "Out of stock";
+            rentButton.classList.add("out-of-stock");
+            rentButton.disabled = true;
+        }
+
+        rentCell.appendChild(rentButton);
+     
+        row.appendChild(nameCell);
+        row.appendChild(genreCell);
+        row.appendChild(priceCell);
+        row.appendChild(stockCell);
+        row.appendChild(rentCell);
+    
+        table.appendChild(row);
+    })   
+}
+
+function handleRentClick (movie, rentButton, stockImage) {
+    
+    const index = movieList.findIndex((m) => m.movieName === movie.movieName);
+             
+    if (index !== -1 && movieList[index].countInStock > 0) {
+        movieList[index].countInStock--;
+        if (localStorage.getItem("yourMovies")) {
+            yourMovies = JSON.parse(localStorage.getItem("yourMovies"));
+            
+        }
+        yourMovies.push(movie);
+        localStorage.setItem("yourMovies", JSON.stringify(yourMovies));
+        localStorage.setItem("movieList", JSON.stringify(movieList));
+        console.log(yourMovies);
+
+        if (movieList[index].countInStock == 0) {
+            rentButton.textContent = "Out of stock";
+            rentButton.classList.add("out-of-stock");
+            rentButton.disabled = true;
+            stockImage.src = "../img/Cross.png";
+        }
     } else {
         rentButton.textContent = "Out of stock";
         rentButton.classList.add("out-of-stock");
         rentButton.disabled = true;
     }
+}
 
-    rentCell.appendChild(rentButton);
-
-    row.appendChild(nameCell);
-    row.appendChild(genreCell);
-    row.appendChild(priceCell);
-    row.appendChild(stockCell);
-    row.appendChild(rentCell);
-
-    table.appendChild(row);
-
-})
-
+renderMovieList(movieList);
---
+++ Scripts/yourMovies.js
@@ -0,0 +1,119 @@
+const yourMovies = JSON.parse(localStorage.getItem("yourMovies")) || [];
+const movieList = JSON.parse(localStorage.getItem("movieList"));
+console.log(yourMovies);
+console.log(movieList);
+
+const table = document.querySelector(".content-table tbody");
+console.log(table);
+const rentTimeAmount = 12;
+
+function renderYourMoviesList (yourMovies) {
+    yourMovies.forEach((movie) => {
+        const row = document.createElement("tr");
+            
+        const nameCell = document.createElement("td");
+        nameCell.textContent = movie.movieName;
+        
+        const genreCell = document.createElement("td");
+        genreCell.textContent = movie.movieGenre;
+        
+        const timeCell = document.createElement("td");
+        timeCell.className = "count-display";
+
+        const countButtonDecrement = document.createElement("button");
+        countButtonDecrement.className = "decrement-btn";
+        countButtonDecrement.innerHTML = "&lt;";
+
+        const rentTime = document.createElement("span");
+        rentTime.textContent = rentTimeAmount + "h";
+
+        const countButtonInrement = document.createElement("button");
+        countButtonInrement.className = "increment-btn";
+        countButtonInrement.innerHTML = "&gt;";
+
+        timeCell.appendChild(countButtonDecrement);
+        timeCell.appendChild(rentTime);
+        timeCell.appendChild(countButtonInrement);
+        
+        const priceCell = document.createElement("td");
+        priceCell.textContent = movie.rentalPrice.toFixed(2) + "€";
+
+        const deleteCell = document.createElement("td");
+        const deleteButton = document.createElement("button");
+        deleteButton.className = "delete-btn";
+        deleteButton.textContent = "Remove";
+
+        deleteCell.appendChild(deleteButton);
+     
+        row.appendChild(nameCell);
+        row.appendChild(genreCell);
+        row.appendChild(timeCell);
+        row.appendChild(priceCell);
+        row.appendChild(deleteCell);
+    
+        table.appendChild(row);
+    })
+}
+
+function handleDeleteMovie (yourMovies) {
+    const deleteButtons = document.querySelectorAll(".delete-btn");
+    deleteButtons.forEach((deleteButton) => {
+        deleteButton.addEventListener("click", () => {
+            const row = deleteButton.closest("tr");
+            const deletedMovieName = row.querySelector("td:first-child").textContent;
+            
+            const index = yourMovies.findIndex((movie) => movie.movieName === deletedMovieName);
+    
+            if (index !== -1) {
+                yourMovies.splice(index, 1);
+                localStorage.setItem("yourMovies", JSON.stringify(yourMovies));
+                table.removeChild(row);
+
+                const deletedMovieInList = movieList.find((movie) => deletedMovieName === movie.movieName);
+                if (deletedMovieInList) {
+                    deletedMovieInList.countInStock++;
+                }
+                localStorage.setItem("movieList", JSON.stringify(movieList));
+            }
+        });
+    })
+}
+
+function handleRentTimeChanges () {
+    const decrementBtns = document.querySelectorAll(".decrement-btn");
+    const incrementBtns = document.querySelectorAll(".increment-btn");
+    
+    decrementBtns.forEach((decrementBtn) => {
+        decrementBtn.addEventListener("click", () => {
+            const countDisplay = decrementBtn.closest(".count-display");
+            const rentTimeText = countDisplay.querySelector("span");
+            let currentRentTime = parseInt(rentTimeText.textContent);
+            if (currentRentTime > 12) {
+                currentRentTime -= 12;
+                rentTimeText.textContent = currentRentTime + "h";
+            } else if (currentRentTime === 12) {
+                alert("This is the minimum of the rent time!")
+            };
+        });
+    });
+
+    incrementBtns.forEach((incrementBtn) => {
+        incrementBtn.addEventListener("click", () => {
+            const countDisplay = incrementBtn.closest(".count-display");
+            const rentTimeText = countDisplay.querySelector("span");
+            let currentRentTime = parseInt(rentTimeText.textContent);
+            if (currentRentTime < 168) {
+                currentRentTime += 12;
+                rentTimeText.textContent = currentRentTime + "h";
+            } else if (currentRentTime === 168) {
+                alert("You reached the maximum of the rent time!")
+            };
+        });
+    });
+
+
+}
+
+renderYourMoviesList(yourMovies);
+handleDeleteMovie(yourMovies);
+handleRentTimeChanges();
